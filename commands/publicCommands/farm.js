1c3228a559393be2e705ddde9ebc0c007fc46ea7
Kervy Cantos <krteammain@gmail.com>
2023-08-11 19:13:47 +0800

    optimized farm command

--- commands/publicCommands/farm.js
+++ commands/publicCommands/farm.js
@@ -12,11 +12,22 @@ module.exports = {
   async execute(interaction) {
     const monsterId = 1;
     const { status, userData } = await registerUser(interaction.member.id);
+    const { data: battleLog } = await supabase
+      .from("ActiveBattleLogs")
+      .select()
+      .eq("userId", interaction.member.id)
+      .eq("monsterId", monsterId)
+      .eq("isActive", true)
+      .order("timeStamp", { ascending: false });
+    console.log(battleLog);
+    let monsterData = [];
+
     if (status == true) {
       return interaction.editReply("You are not registered");
     }
     try {
       await interaction.deferReply();
+
       const { data: monsterData } = await supabase
         .from("Monsters")
         .select()
@@ -26,33 +37,75 @@ module.exports = {
         .setTitle(monsterData[0].name)
         .addFields(
           { name: "LvL", value: String(monsterData[0].lvl), inline: true },
-          { name: "Health", value: String(monsterData[0].hp), inline: true }
+          {
+            name: "Health",
+            value:
+              battleLog.length > 0
+                ? String(battleLog[0]?.remainingHp)
+                : String(monsterData[0].hp),
+            inline: true,
+          }
         )
         .setColor("Red");
       const message = await interaction.editReply({
         embeds: [embed],
         fetchReply: true,
       });
-      message.react("⚔️");
+      const attackLogs = [];
+      const fetchAttackersData = async (arr) => {
+        const { data: attackersData } = await supabase
+          .from("Users")
+          .select()
+          .in("discordId", arr);
+        return attackersData;
+      };
+      await message.react("⚔️");
+
       const filter = (reaction, user) => {
-        return (
-          reaction.emoji.name === "⚔️" &&
-          user.id != interaction.client.application.id
-        );
+        return reaction.emoji.name === "⚔️";
       };
 
       return message
-        .awaitReactions({ filter, max: 1, time: 10000, errors: ["time"] })
-        .then((collected) => {
-          embed.setFooter({
-            text: `You attacked ${monsterData[0].name}`,
-            iconURL: `${monsterData[0].imageUrl}`,
-          });
-          interaction.editReply({ embeds: [embed], ephemeral: true });
-          message.reactions.removeAll();
+        .awaitReactions({ filter, time: 10000 })
+        .then(async (collected) => {
+          const reaction = collected.first();
+
+          if (reaction) {
+            const attackers = reaction.users.cache.filter(
+              (user) => user.bot != true
+            );
+            const attackerIds = attackers.map((d) => d.id);
+            const attackersData = await fetchAttackersData(attackerIds);
+
+            let monsterHp =
+              battleLog.length > 0
+                ? battleLog[0]?.remainingHp
+                : monsterData[0].hp;
+            attackersData.map(async (user) => {
+              monsterHp -= user.maxDmg;
+              embed.addFields({
+                name: `Attackers:`,
+                value: user.discordUserName,
+                inline: false,
+              });
+            });
+            embed.data.fields[1] = {
+              name: "Health",
+              value: `${monsterHp}`,
+            };
+            message.reactions.removeAll();
+            interaction.editReply({ embeds: [embed], ephemeral: true });
+            const logs = {
+              monsterId: monsterData[0].id,
+              userId: interaction.member.id,
+              remainingHp: monsterHp,
+              isActive: true,
+            };
+            return await supabase.from("ActiveBattleLogs").insert(logs);
+          }
         })
         .catch((collected) => {
-          console.log("no reactions");
+          console.log(collected);
         });
     } catch (error) {
       console.log(error);
